cd82497a32b89397e94eec06c036b461a235bbe0
Adam The Canadian <adam.jarrett.clare@gmail.com>
2021-09-28 19:00:48 -0600

    Add first example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include "immintrin.h"
+
+/**
+ * This is what a __m128 looks like, or can be thought of
+ * It contains 4 floats in contiguous order
+ */
+union DummyUnion128 {
+  __m128 f;
+  float f4[4];
+} ;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  DummyUnion128 m1;
+  DummyUnion128 m2;
+  m1.f = _mm_set_ps(4.0f, 3.0f, 2.0f, 1.0f);
+  m2.f = _mm_set_ps(8.0f, 7.0f, 6.0f, 5.0f);
+
+  printf("Notice the order of f4 is in reverse order we initialized within _mm_set_ps\n");
+  for (int i = 0; i < 4; i++) {
+    printf("m1.f4[%d]: %.2f ", i, m1.f4[i]);
+    printf("m2.f4[%d]: %.2f ", i, m2.f4[i]);
+    printf("\n");
+  }
+
+  __m128 s = _mm_add_ps(m1.f, m2.f);
+  printf("\nThe sum is: ");
+  for (int i = 0; i < 4; i++) {
+    printf("%.3f, ", s[i]);
+  }
+  printf("\n");
   return 0;
 }
---
+++ readme.md
@@ -0,0 +1,30 @@
+# SIMD Practice 
+Practicing working with SIMD instructions 
+
+## Tutorials/guides
+List of tutorials/guides/websites that I am following
+
+- http://www.cs.uu.nl/docs/vakken/magr/2017-2018/files/SIMD%20Tutorial.pdf
+- https://software.intel.com/sites/landingpage/IntrinsicsGuide/
+
+## Notes
+
+- __mm_set_ps or _mm_add_ps
+    - called intrinsics 
+    - All compile to a single instruction
+    
+- _mm_sub_ps
+    - Subtract
+- _mm_mul_ps
+    - multiply
+- _mm_div_ps
+    - Division
+- _mm_sqrt_ps
+    - sqrt
+- _mm_rcp_ps
+    - reciprocal
+
+- SSE
+  - 128 bit level instructions
+- AVX
+  - Added 256 (and maybe more) bit level instructions
